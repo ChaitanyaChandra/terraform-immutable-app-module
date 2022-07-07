3d3b80157198f4bb1a09225b38bda280f8409557
ChaitanyaChandra <chay@outlook.in>
2022-07-07 13:27:33 +0530

    automated commit #1338

--- datadog.sh
+++ datadog.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -f /etc/nginx/conf.d/nodejs.conf ]; then
-  hostnamectl set-hostname frontend-dev -skip-apply
+  hostnamectl set-hostname frontend-dev
 fi
 
 if [ -d /home/node ]; then
-  hostnamectl set-hostname spec-dev -skip-apply
+  hostnamectl set-hostname spec-dev
 fi
 
 DD_API_KEY="db2fa345cffeb71eeac6db8317b4b29b" DD_SITE="datadoghq.com" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
